7fbca7dd322796674ac3908d2c564fe68782427d
francisco <franciscojn49@gmail.com>
2019-08-19 11:15:41 -0300

    main com stack

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication
 from telas.tela_inicial import Ui_Tela_Inicio
 from telas.tela_cadastro import Ui_Tela_Cadastro
 from telas.tela_cadastro_livro import Ui_Tela_Cadastro_Livro
-
+from telas.tela_principal import Ui_Tela_Principal
+import sys
+import os
 from PyQt5.QtCore import pyqtSlot
 import fireBaseSD
 
@@ -18,52 +20,102 @@ config = {
 }
 
 
-class Tela_Inicio(QMainWindow):
+# class Tela_Inicio(QMainWindow):
 
-    def __init__(self):
-        super().__init__()
-        self.firebase = fireBaseSD.initFb(config)
-        self.ui = Ui_Tela_Inicio()
-        self.ui.setupUi(self)
-        self.ui.botao_entrar.clicked.connect(self.entrar)
-        self.ui.botao_criar_conta.clicked.connect(self.abrir_criar_conta)
-        self.show()
-
-
-    def entrar(self):
-        if(self.firebase.login(self.ui.e_mail_login.text(), self.ui.senha_login.text()) == 'Ok'):
-            print('login feito')
-        else:
-            print('erro')
-        
-    def abrir_criar_conta(self):
-        Tela_Cadastro().__init__()
-        # self.ui.setupUi(self)
-
-class Tela_Cadastro(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-        self.tela = Ui_Tela_Cadastro()
-        self.tela.setupUi(self)
-        self.showw()
-        self.show()
-    # def cirar_conta(self):
-
-    def showw(self):
-        print('aqui')
-
-
-# class Tela_Cadastro_Livro(QMainWindow):
 #     def __init__(self):
 #         super().__init__()
+#         self.firebase = fireBaseSD.initFb(config)
+#         self.ui = Ui_Tela_Inicio()
+#         self.ui.setupUi(self)
+#         self.ui.botao_entrar.clicked.connect(self.entrar)
+#         self.ui.botao_criar_conta.clicked.connect(self.abrir_criar_conta)
+#         self.show()
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    # MainWindow = QtWidgets.QMainWindow()
-    ui = Tela_Inicio()
-    # ui.setupUi(MainWindow)
-    # MainWindow.show()
+#     def entrar(self):
+#         # if(self.firebase.login(self.ui.e_mail_login.text(), self.ui.senha_login.text()) == 'Ok'):
+#         #     print('login feito')
+#         # else:
+#         #     print('erro')
+
+#         if(self.firebase.login(self.ui.e_mail_login.text(), self.ui.senha_login.text()) == 'Ok'):
+#             QMessageBox.about(self,'Deu certo','Eu foda!!')
+#         else:
+#             QMessageBox.about(self,'Erro','E-mail ou senha incorretos!!')
+        
+
+#     def abrir_criar_conta(self):
+#         Tela_Cadastro().__init__()
+#         # self.ui.setupUi(self)
+
+# class Tela_Cadastro(QMainWindow):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.tela = Ui_Tela_Cadastro()
+#         self.tela.setupUi(self)
+#         self.showw()
+#         self.show()
+#     # def cirar_conta(self):
+
+#     def showw(self):
+#         print('aqui')
+
+
+# # class Tela_Cadastro_Livro(QMainWindow):
+# #     def __init__(self):
+# #         super().__init__()
+
+
+# if __name__ == "__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     # MainWindow = QtWidgets.QMainWindow()
+#     ui = Tela_Inicio()
+#     # ui.setupUi(MainWindow)
+#     # MainWindow.show()
+#     sys.exit(app.exec_())
+
+class Ui_Main(QtWidgets.QWidget):
+    def setupUi(self, Main):
+        Main.setObjectName('Main')
+        Main.resize(1200, 900)
+
+        self.QtStack = QtWidgets.QStackedLayout()
+
+        self.stack0 = QtWidgets.QMainWindow()
+        self.stack1 = QtWidgets.QMainWindow()
+        self.stack2 = QtWidgets.QMainWindow()
+        self.stack3 = QtWidgets.QMainWindow()
+
+        self.tela_inicio = Ui_Tela_Inicio()
+        self.tela_inicio.setupUi(self.stack0)
+
+        self.tela_cadastro = Ui_Tela_Cadastro()
+        self.tela_cadastro.setupUi(self.stack1)
+
+        self.tela_principal = Ui_Tela_Principal()
+        self.tela_principal.setupUi(self.stack2)
+
+        self.tela_cadastro_livro = Ui_Tela_Cadastro_Livro()
+        self.tela_cadastro_livro.setupUi(self.stack3)
+
+        self.QtStack.addWidget(self.stack0)
+        self.QtStack.addWidget(self.stack1)
+        self.QtStack.addWidget(self.stack2)
+        self.QtStack.addWidget(self.stack3)
+
+class Main(QMainWindow, Ui_Main):
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        self.setupUi(self)
+
+        self.tela_inicio.botao_criar_conta.clicked.connect(self.criar_conta)
+
+    def criar_conta(self):
+        self.QtStack.setCurrentIndex(1)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    show_main = Main()
     sys.exit(app.exec_())
